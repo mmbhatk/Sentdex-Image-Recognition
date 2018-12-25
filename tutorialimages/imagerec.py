6fa60b3c51dadaae1c257fcc9e9032fcab919d35
mmbhatk <kmanasvibhat@gmail.com>
2018-12-25 14:57:37 +0530

    Final.

--- tutorialimages/imagerec.py
+++ tutorialimages/imagerec.py
@@ -1,94 +1,56 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-from functools import reduce
 from statistics import mean
 from collections import Counter
 
 def createExamples():
-    numberArrayExamples = open('numArrEx.txt', 'a')
-    numbersWeHave = range(0, 10)
-    versionsWeHave = range(1, 10)
-    for eachNum in numbersWeHave:
-        for eachVer in versionsWeHave:
-            imgFilePath = 'images/numbers/' + str(eachNum) + '.' + str(eachVer) + '.png'
-            ei = Image.open(imgFilePath)
-            eiar = str(np.array(ei).tolist())
-            numberArrayExamples.write(str(eachNum) + '::' + eiar + '\n')
+    numbers = open('numberArr', 'a')
+    for i in range(0, 10):
+        for j in range(1, 10):
+            img = Image.open('images/numbers/' + str(i) + '.' + str(j) + '.png')
+            iar = str(np.array(img).tolist())
+            numbers.write(str(i) + '::' + iar + '\n')
 
 def threshold(imageArray):
-    balanceAr = []
-    newAr = imageArray
+    balanceArr = []
+    newArr = imageArray
     for eachRow in imageArray:
         for eachPix in eachRow:
-            avgNum = reduce(lambda x, y: x + y, eachPix[:3]) / len(eachPix[:3])
-            balanceAr.append(avgNum)
-    balance = reduce(lambda x, y: x + y, balanceAr) / len(balanceAr)
-    for eachRow in newAr:
+            balanceArr.append(mean(eachPix[:3]))
+    balance = mean(balanceArr)
+    for eachRow in newArr:
         for eachPix in eachRow:
-            if reduce(lambda x, y: x + y, eachPix[:3]) / len(eachPix[:3]) > balance:
-                eachPix[0] = 255
-                eachPix[1] = 255
-                eachPix[2] = 255
-                eachPix[3] = 255
+            if mean(eachPix[:3]) < balance:                
+                eachPix[0], eachPix[1], eachPix[2], eachPix[3] = 255, 255, 255, 255
             else:
-                eachPix[0] = 0
-                eachPix[1] = 0
-                eachPix[2] = 0
-                eachPix[3] = 255
-    return newAr
-
-def whatNumIsThis2(filePath):
-    matchedArr = []
-    loadExamples = open('numArrEx.txt', 'r').read()
-    loadExamples = loadExamples.split('\n')
-    
-    i = Image.open(filePath)
-    iar = np.array(i)
-    iar1 = iar.tolist()
-    inQuestion = str(iar1)
-    
-    for eachExample in loadExamples:
-        try:
-            splitExample = eachExample.split('::')
-            currentNum = splitExample[0]
-            currentArr = splitExample[1]
-            eachPixExample = currentArr.split('],')
-            eachPixInQ = inQuestion.split('],')
+                eachPix[0], eachPix[1], eachPix[2], eachPix[3] = 0, 0, 0, 255
             
-            x = 0
-            while x<len(eachPixExample):
-                if eachPixExample[x] == eachPixInQ[x]:
-                    matchedArr.append(int(currentNum))
-                x+=1
-        except Exception as e:
-            print(str(e))
-    x = Counter(matchedArr)
-    graphX = []
-    graphY = []
-
-    ylimi = 0
-
-    for eachThing in x:
-        graphX.append(eachThing)
-        graphY.append(x[eachThing])
-        ylimi = x[eachThing]
-
-
-
-    fig = plt.figure()
+def identifyNum(filePath):
+    counts = []
+    img = Image.open(filePath)
+    i_np = np.array(img)
+    iar = str(i_np.tolist())
+    iar2 = iar.split('],')
+    
+    loadExamples = open('numberArr', 'r').read().split('\n')
+    for example in loadExamples:
+        if len(example) > 1:
+            num = example[0]
+            pixels = example[3:].split('],')
+            for i in range(len(iar2)):
+                if iar2[i] == pixels[i]:
+                    counts.append(int(num))
     ax1 = plt.subplot2grid((4,4),(0,0), rowspan=1, colspan=4)
-    ax2 = plt.subplot2grid((4,4),(1,0), rowspan=3,colspan=4)
-    
-    ax1.imshow(iar)
-    ax2.bar(graphX,graphY,align='center')
-    plt.ylim(400)
-    
-    xloc = plt.MaxNLocator(12)
-    ax2.xaxis.set_major_locator(xloc)
-
+    ax2 = plt.subplot2grid((4,4),(1,0), rowspan=3, colspan=4)
+    print(Counter(counts))
+    counts = sorted(Counter(counts).items())
+    x, y = zip(*counts)
+    ax2.bar(x, y)
+    ax1.imshow(i_np)
     plt.show()
-    print(x)
-
-
-whatNumIsThis('test.png')
+    
+img = Image.open('test.png')
+iar = np.array(img)
+threshold(iar)
+identifyNum('test.png')
